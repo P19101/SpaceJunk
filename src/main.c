a24a315ecba5da0e98adac9161e5b3b1c4e52914
P19101 <ans4658@rit.edu>
2019-03-28 23:00:21 +0100

    added the main file to the repo. as noted it was a local file only in the project.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,35 @@
+#include <msp430.h> 
+#include "systemInit.h"
+#include "gpio.h"
 #include "msp430fr5969.h"
+#include "uart.h"
+#include "USCITypeA.h"
+#include <stdio.h>
+/**
+ * main.c
+ */
 
-void main(void){
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	configure_sys();
+	uint8_t character = 0x31;
+	hold_USCIA_UART_reset();
+	choose_USCIA_operation_mode(UART_MODE);
+	set_clock_source_USCIA_UART(USCIA_SMCLK);
+	select_baud_rate_UART(BAUD_9600);
+	//UCA0CTL0 |= UCMSB;
+	release_USCIA_UART_reset();
+	while(1){
+	    while(read_TX_interrupt_flag() == 0);
+	    send_byte_UART(character);
+	    while(read_RX_interrupt_flag() == 0);
+	    character = read_byte_UART();
+	}
+
+
+
+
+	return 0;
 }
